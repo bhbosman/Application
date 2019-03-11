0dff7ed8f18e87513935ab6f03be6ed94171d4af
Brendan Bosman <bhbosman@gmail.com>
2019-03-11 15:20:11 +0200

    Some changes

--- goidlgenerator/Publish/publishGo/publishEnum.go
+++ goidlgenerator/Publish/publishGo/publishEnum.go
@@ -65,6 +65,36 @@ func (self publishEnum) Export(writer io.StringWriter, TypeInformation interface
 			typeCode:        typeCode,
 			defaultValue:    "0",
 		})
+
+	self.GenerateWriteFunction(writer, TypeInformation, typeNamePrefix, typeCode)
+	self.GenerateReadFunction(writer, TypeInformation, typeNamePrefix, typeCode)
+}
+
+
+func (self publishEnum) GenerateReadFunction(writer io.StringWriter, TypeInformation interfaces.IBaseTypeInformation, typeNamePrefix string, typeCode uint32) {
+	returnType := "*" + typeNamePrefix
+	_, _ = writer.WriteString(fmt.Sprintf("// %v reader\n", typeNamePrefix))
+	_, _ = writer.WriteString(fmt.Sprintf("func Read_%v(stream Streams.I%vReader) (value %v, byteCount int, err error) {\n", typeNamePrefix, TypeInformation.Name(), returnType))
+
+	_, _ = writer.WriteString(fmt.Sprintf("\tvalue = New%v()\n", typeNamePrefix))
+
+	_, _ = writer.WriteString(fmt.Sprintf("\tvar n int \n"))
+
+
+	_, _ = writer.WriteString(fmt.Sprintf("\treturn value, byteCount, nil\n"))
+	_, _ = writer.WriteString(fmt.Sprintf("}\n"))
+	_, _ = writer.WriteString(fmt.Sprintf("\n"))
+}
+
+func (self publishEnum) GenerateWriteFunction(writer io.StringWriter, TypeInformation interfaces.IBaseTypeInformation, typeNamePrefix string, typeCode uint32) {
+	returnType := "*" + typeNamePrefix
+	_, _ = writer.WriteString(fmt.Sprintf("// %v writer \n", typeNamePrefix))
+	_, _ = writer.WriteString(fmt.Sprintf("func Write_%v(stream Streams.I%vWriter, value %v) (byteCount int, err error) {\n", typeNamePrefix, TypeInformation.Name(), returnType))
+	_, _ = writer.WriteString(fmt.Sprintf("\tvar n int \n"))
+
+	_, _ = writer.WriteString(fmt.Sprintf("\treturn byteCount, nil\n"))
+	_, _ = writer.WriteString(fmt.Sprintf("}\n"))
+	_, _ = writer.WriteString(fmt.Sprintf("\n"))
 }
 
 func NewPublishEnum(data *yacc.EnumDecl, typeInformation interfaces.IBaseTypeInformation) *publishEnum {
